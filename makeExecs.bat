bf58b0cdc0f4890a2cf702a64638ac424948f164
Raveman <j2dbparser@gmail.com>
2010-05-02 22:18:58 +0200

    launch4j-xml-plugin

--- makeExecs.bat
+++ makeExecs.bat
@@ -3,7 +3,8 @@ cls
 call mvn -o package
 call "C:\Program Files\Launch4j\launch4j.exe" J:\workspace\j2DbParse\configParserLive.xml
 call "C:\Program Files\Launch4j\launch4j.exe" J:\workspace\j2DbParse\configParser.xml
+cd target
 call parser.exe
 call parserLive.exe
-
+cd ..
 pause
