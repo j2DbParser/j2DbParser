2d9ff675c213953c0b3ee45b0673dde8ff6a84a3
Raveman <j2dbparser@gmail.com>
2010-05-08 19:08:55 +0200

    logger configuration

---
+++ src/main/java/j2DbParser/system/LogFactory.java
@@ -0,0 +1,40 @@
+package j2DbParser.system;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class LogFactory {
+	private static final String LOGGING_PROPERTIES = "logging.properties";
+
+	static {
+		loadFromProperties();
+	}
+
+	private static void loadFromProperties() {
+		try {
+			InputStream stream = null;
+			try {
+				stream = ClassLoader
+						.getSystemResourceAsStream(LOGGING_PROPERTIES);
+				LogManager.getLogManager().readConfiguration(stream);
+			} finally {
+				if (stream != null) {
+					stream.close();
+				}
+			}
+		} catch (SecurityException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static Logger getLogger(Class<?> clazz) {
+		System.out.println("getLogger(" + clazz + ")");
+		Logger log = Logger.getLogger(clazz.getName());
+		return log;
+	}
+
+}
