1b769c67f2a8825d4ce3814c2e018fcec7a1b950
Raveman <j2dbparser@gmail.com>
2010-05-08 19:08:04 +0200

    example sql

--- hsql.sql
+++ hsql.sql
@@ -1,9 +1,17 @@
-SELECT count(*) FROM log
-SELECT * FROM log
-create table bla2 (class DATE);
-SELECT * FROM bla2
-insert into bla2 (class) values ('2010-03-19');
-insert into bla2 (class) values ('2010-03-19 14:30:05,076');
+14,5MB
+added=21012
 
-create table bla3 (class TIME);
-insert into bla3 (class) values ('14:30:05 2010-03-19');
+select level, max(time) as max_time, count(*) as how_many from log group by log.level order by how_many desc
+LEVEL                                                                 MAX_TIME                                                              HOW_MANY
+DEBUG                                                                 2010-03-19 15:43:28,654                                               19104
+INFO                                                                  2010-03-19 15:30:42,122                                               1784
+ERROR                                                                 2010-03-19 15:02:49,388                                               82
+WARN                                                                  2010-03-19 15:30:42,435                                               42
+
+
+select level, max(time) as max_time, min(time) as min_time, count(*) as how_many from log group by log.level order by how_many desc
+LEVEL                                                                 MAX_TIME                                                              MIN_TIME                                                              HOW_MANY
+DEBUG                                                                 2010-03-19 15:43:28,654                                               2010-03-19 14:30:16,841                                               19104
+INFO                                                                  2010-03-19 15:30:42,122                                               2010-03-19 14:29:33,919                                               1784
+ERROR                                                                 2010-03-19 15:02:49,388                                               2010-03-19 14:33:24,685                                               82
+WARN                                                                  2010-03-19 15:30:42,435                                               2010-03-19 14:29:58,310                                               42
